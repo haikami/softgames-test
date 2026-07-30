969888f2462052bced728b3f2cf7a77a18c3005d
Hayk Papoyan <imhayk@gmail.com>
2026-07-30 08:50:17 +0200

    improved main menu layout

---
+++ Assets/Scripts/Core/UI/MenuButtonsLayoutHandler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Core.UI
+{
+    /// <summary>
+    /// Changes menu buttons layout when aspect ratio gets past a threshold
+    /// </summary>
+    public sealed class MenuButtonsLayoutHandler : MonoBehaviour
+    {
+        [SerializeField] private GridLayoutGroup _gridLayout;
+        [SerializeField] private float _landscapeAspectThreshold = 1.3f;
+
+        private bool? _isLandscape;
+
+        private void Awake()
+        {
+            RefreshLayout(force: true);
+        }
+
+        private void Update()
+        {
+            RefreshLayout(force: false);
+        }
+
+        private void RefreshLayout(bool force)
+        {
+            var isLandscape = (float)Screen.width / Screen.height >= _landscapeAspectThreshold;
+
+            if (!force && _isLandscape == isLandscape)
+                return;
+
+            _isLandscape = isLandscape;
+
+            _gridLayout.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+            _gridLayout.constraintCount = isLandscape ? 2 : 1;
+        }
+    }
+}
